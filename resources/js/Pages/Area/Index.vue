376ca03f7ddf8881b4e9f6278450bdbf44795102
IamShaDoW666 <www.gaming2018@gmail.com>
2022-06-07 23:50:28 +0530

    fix table z-index in Areas/Index.vue

--- resources/js/Pages/Area/Index.vue
+++ resources/js/Pages/Area/Index.vue
@@ -1,45 +1,61 @@
 <template>
-  <Admin :headerStats="false" >
+  <Admin :headerStats="false">
+
     <Head title="Areas" />
     <HeadlessArea />
     <HeadlessAreaEdit v-if="areaOpenEdit" />
     <div class="relative flex mt-2 sm:mt-4
        flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
-       <div class="rounded-t bg-white mb-0 px-6 py-6">
-          <div class="text-center flex justify-between">
-             <h6 class="text-gray-700 text-xl font-bold">Delivery Areas</h6>
-             <button @click="areaOpen = true" class="px-4 py-2 text-sm rounded shadow bg-blue-500 text-white hover:bg-blue-700">Create</button>
-          </div>
-       </div>
-       <div class="flex-auto px-4 lg:px-10 py-10">
-         <table class="min-w-full border-collapse block md:table rounded-lg">
-           <thead class="block md:table-header-group">
-             <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-               <th class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">ID</th>
-               <th class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">Area</th>
-               <th class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">Delivery Charge</th>
-               <th class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
-             </tr>
-           </thead>
-           <tbody class="block md:table-row-group">
-             <tr v-for="(area, index) in areas" class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
-               <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">No:</span>{{ index+1 }}</td>
-               <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Area:</span>{{ area.name }}</td>
-               <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                 <span class="inline-block w-1/3 md:hidden font-bold">
-                   Delivery Charge:
-                 </span>
-                 {{ area.delivery_fee }}
-               </td>
-               <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                 <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
-                 <button @click="openAreaEdit(area)" class="ml-2 mr-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
-                <button @click="deleteArea(area.id)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
-               </td>
-             </tr>
-           </tbody>
-         </table>
-       </div>
+      <div class="rounded-t bg-white mb-0 px-6 py-6">
+        <div class="text-center flex justify-between">
+          <h6 class="text-gray-700 text-xl font-bold">Delivery Areas</h6>
+          <button @click="areaOpen = true"
+            class="px-4 py-2 text-sm rounded shadow bg-blue-500 text-white hover:bg-blue-700">Create</button>
+        </div>
+      </div>
+      <div class="flex-auto px-4 lg:px-10 py-10">
+        <table class="min-w-full border-collapse block md:table rounded-lg">
+          <thead class="block md:table-header-group">
+            <tr
+              class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+              <th style="z-index: 0!important"
+                class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                ID</th>
+              <th
+                class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                Area</th>
+              <th
+                class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                Delivery Charge</th>
+              <th
+                class="bg-gray-600 p-2 text-slate-500 font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                Actions</th>
+            </tr>
+          </thead>
+          <tbody class="block md:table-row-group">
+            <tr v-for="(area, index) in areas"
+              class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+              <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span
+                  class="inline-block w-1/3 md:hidden font-bold">No:</span>{{ index+1 }}</td>
+              <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span
+                  class="inline-block w-1/3 md:hidden font-bold">Area:</span>{{ area.name }}</td>
+              <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                <span class="inline-block w-1/3 md:hidden font-bold">
+                  Delivery Charge:
+                </span>
+                {{ area.delivery_fee }}
+              </td>
+              <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
+                <button @click="openAreaEdit(area)"
+                  class="ml-2 mr-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+                <button @click="deleteArea(area.id)"
+                  class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </Admin>
 </template>
