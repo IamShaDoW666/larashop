2391d2db584f90c19bb07578e05f3dce023e2ecc
IamShaDoW666 <www.gaming2018@gmail.com>
2022-06-09 14:43:46 +0530

    update sweetalert @ admin.vue

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -45,17 +45,17 @@ export default {
           const Toast = Swal.mixin({            
             toast: true,
             position: 'top-end',
-            iconColor: 'white',
-            customClass: {
-              popup: 'colored-toast'
-            },
+            // iconColor: 'white',
+            // customClass: {
+            //   popup: 'colored-toast'
+            // },
             showConfirmButton: false,
-            // timer: 3000,
-            // timerProgressBar: true,
-            // didOpen: (toast) => {
-            //   toast.addEventListener('mouseenter', Swal.stopTimer)
-            //   toast.addEventListener('mouseleave', Swal.resumeTimer)
-            // }
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
           })
 
           //Mutate Response
