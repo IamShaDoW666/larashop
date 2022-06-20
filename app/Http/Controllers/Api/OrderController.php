8d0faea0fd0360333496a2546973d2e5f876582d
IamShaDoW666 <www.gaming2018@gmail.com>
2022-06-21 03:02:53 +0530

    added update order status via api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Http\Request;
+
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
@@ -15,5 +17,42 @@ class OrderController extends Controller
             ->with('products')
             ->orderBy('created_at', 'DESC')
             ->get());
+    }
+
+    public function updateStatus(Request $request, Order $order)
+    {
+        $status = $request->current_status;
+        $action = $request->action;
+
+        // Check Reject 
+        if (!$action && $status == 'pending') {
+            $order->status = 'rejected';
+            $order->save();
+            return 'Order Rejected!';
+        }
+
+        // If positive action
+        if ($action) {
+            switch ($status) {
+                case 'pending':
+                    $order->status = 'accepted';
+                    $order->save();
+                    break;
+                case 'accepted':
+                    $order->status = 'prepared';
+                    $order->save();
+                    break;
+                case 'prepared':
+                    $order->status = 'delivered';
+                    $order->save();
+                    break;
+                case 'delivered':
+                    $order->status = 'closed';
+                    $order->save();
+                    break;
+            }
+
+            return 'Order status updated!';
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/get-orders', [OrderController::class, 'index']);
+Route::post('/update-order-status/{order}', [OrderController::class, 'updateStatus']);
 
