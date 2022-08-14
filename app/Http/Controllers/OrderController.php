bf116d43d46a96168eaa9e20d9e33d9f2c9b6c09
IamShaDoW666 <www.gaming2018@gmail.com>
2022-08-14 12:38:14 +0530

    fix amount in order.show

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -168,7 +168,7 @@ class OrderController extends Controller
     public function show($order)
     {
         $order = OrderResource::make(Order::find($order)->load('products'));
-        return $order;
+        // return $order;
         return inertia('Order/Show', compact('order'));
     }
 
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -30,7 +30,7 @@ class ProductResource extends JsonResource
 
     $subtotalFormatted = null;
     $subtotal = null;
-    if ($pivot_qty && $pivot_qty != null) {
+    if ($pivot_qty && (is_string($pivot_qty) || is_integer($pivot_qty))) {
       if ($variant_price_int) {
         $subtotal = $variant_price_int * $pivot_qty;
       } else {
